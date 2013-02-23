72b39f6dd7a2795766f9fa18ee9d4eb7fb585a63
rvazquezglez <rvazquezglez@gmail.com>
2013-02-23 04:55:59 -0600

    Constructor estilo groovy :D

--- groovy/script.groovy
+++ groovy/script.groovy
@@ -2,16 +2,11 @@ public class Persona {
     def nombre;
     def edad;    
     
-    public Persona(nombre, edad){
-        this.nombre = nombre
-        this.edad = edad
-    }
-    
     public String saluda(){
         return "Hola me llamo $nombre y tengo $edad años."
     }            
 }
 
-Persona persona = new Persona("Juan", 21);
+Persona persona = new Persona(nombre:"Juan", edad:21);
 
 println persona.saluda()
