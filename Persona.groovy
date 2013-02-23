a7af018943cc1cd61dc79d93030fa6faa3ef5ada
rvazquezglez <rvazquezglez@gmail.com>
2013-02-23 04:41:13 -0600

    Azúcar! Se quitan ;

--- Persona.groovy
+++ Persona.groovy
@@ -5,29 +5,29 @@ public class Persona {
     public static void main(String[] args){
         Persona persona = new Persona();
         
-        persona.setNombre("Juan");
-        persona.setEdad(21);
+        persona.setNombre("Juan")
+        persona.setEdad(21)
         
-        System.out.println(persona.saluda());
+        System.out.println(persona.saluda())
     }
     
     public String saluda(){
-        return "Hola me llamo " + nombre + " y tengo " + edad + " años.";
+        return "Hola me llamo " + nombre + " y tengo " + edad + " años."
     }
     
     public String getNombre(){
-        return this.nombre;
+        return this.nombre
     }
     
     public void setNombre(String nombre){
-        this.nombre = nombre;
+        this.nombre = nombre
     }
 
     public Integer getEdad(){
-        return this.edad;
+        return this.edad
     }
     
     public void setEdad(Integer edad){
-        this.edad = edad;
+        this.edad = edad
     }    
 }
