d591f466fe0c64b2cd239dedf725950828e9eb06
rvazquezglez <rvazquezglez@gmail.com>
2013-02-23 04:39:37 -0600

    Persona estilo java

---
+++ Persona.groovy
@@ -0,0 +1,33 @@
+public class Persona {
+    private String nombre;
+    private Integer edad;
+    
+    public static void main(String[] args){
+        Persona persona = new Persona();
+        
+        persona.setNombre("Juan");
+        persona.setEdad(21);
+        
+        System.out.println(persona.saluda());
+    }
+    
+    public String saluda(){
+        return "Hola me llamo " + nombre + " y tengo " + edad + " años.";
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad(){
+        return this.edad;
+    }
+    
+    public void setEdad(Integer edad){
+        this.edad = edad;
+    }    
+}
